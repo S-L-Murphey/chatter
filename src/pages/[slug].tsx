bdbbfbde6ce7c42b6f45c33b5f1b18532f62269c
S-L-Murphey <smurphey1990@gmail.com>
2023-06-11 10:02:53 -0500

    fix: vercel error

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -31,13 +31,13 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <div className=" bg-slate-600 h-36 relative">
           <Image
             src={data.profileImageUrl}
-            alt={`${data.username}'s profile picture`}
+            alt={`${data?.username}'s profile picture`}
             height={128}
             width={128}
             className="absolute bottom-0 left-0 -mb-[64px] rounded-full ml-4 border-2 border-black bg-black" />
         </div>
         <div className="h-[64px]" />
-        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="p-4 text-2xl font-bold">{`@${data?.username}`}</div>
         <div className="w-full border-b border-slate-400" />
         <ProfileFeed userId={data.id}/>
       </PageLayout>
