55c3c52464ef637b08b679270f4671ef237a3fea
S-L-Murphey <smurphey1990@gmail.com>
2023-06-13 10:40:37 -0500

    build: user button on sidenav

--- src/components/sidenav.tsx
+++ src/components/sidenav.tsx
@@ -1,5 +1,8 @@
-import { HomeIcon, UserIcon, BeakerIcon, BookmarkIcon, EnvelopeIcon, BellIcon, HashtagIcon, BugAntIcon } from "@heroicons/react/24/solid"
+import { HomeIcon, UserIcon, BeakerIcon, BookmarkIcon, EnvelopeIcon, BellIcon, HashtagIcon, BugAntIcon, EllipsisHorizontalIcon } from "@heroicons/react/24/solid"
 import Link from "next/link"
+import { api } from "~/utils/api";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
 
 
 const NAVIGATION_ITEMS = [
@@ -41,7 +44,7 @@ export const SideNav = () => {
             </Link>
             <div className="flex flex-col items-stretch h-full space-y-4 mt-4">
                 {NAVIGATION_ITEMS.map((item) => (
-                    <Link className="hover:bg-slate-700 transition duration-200 rounded-3xl p-2 py-2 px-6 flex justify-start w-fit items-center space-x-4 text-2xl" href={`/${item.title.toLowerCase()}`} key={item.title}>
+                    <Link className="hover:bg-white/10 transition duration-200 rounded-3xl p-2 py-2 px-6 flex justify-start w-fit items-center space-x-4 text-2xl" href={`/${item.title.toLowerCase()}`} key={item.title}>
                         <div className="h-5 w-5">
                             <item.Icon />
                         </div>
@@ -53,8 +56,36 @@ export const SideNav = () => {
 
                 ))}
                 <button className="w-full rounded-full text-2xl font-bold text-center bg-teal-500 m-4 px-6 py-2 hover:bg-teal-400 text-slate-700 transition duration-200">Chat</button>
-                <div className="absolute inset-x-0 bottom-0 pb-5">User Info Button</div>
+                <div className="absolute inset-x-0 bottom-0 pb-5"><UserInfoButton /></div>
             </div>
         </section>
+    )
+};
+
+const UserInfoButton = () => {
+    const { user } = useUser();
+
+    const { data } = api.profile.getUserByUsername.useQuery({ username: user?.username! })
+
+    if (!data) return <div>No Data</div>
+
+    return (
+        <Link href={`/@${data.username}`}>
+            <button className="flex items-center justify-center rounded-full m-4 p-2.5 text-center bg-transparent space-x-3 border border-slate-900 hover:border-slate-700 hover:bg-white/10">
+                <Image
+                    src={data.profileImageUrl}
+                    alt={`${data?.username}'s profile pic`}
+                    width={52}
+                    height={52}
+                    className="rounded-full" />
+                <div className="">
+                    <div className="text-base font-semibold">{data.username}</div>
+                    <div className="text-xs">{`@${data.username}`}</div>
+                </div>
+                <div><EllipsisHorizontalIcon className="w-5 h-5" /></div>
+
+            </button>
+        </Link>
+
     )
 }
