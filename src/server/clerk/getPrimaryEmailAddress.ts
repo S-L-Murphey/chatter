a74f5282b44f0708caeb279bd9548b7b6093edb7
S-L-Murphey <smurphey1990@gmail.com>
2023-06-28 14:38:03 -0500

    chore: remove all webhook related code:

--- src/server/clerk/getPrimaryEmailAddress.ts
+++
@@ -1,7 +0,0 @@
-import type { UserJSON } from '@clerk/types';
-
-export function getPrimaryEmailAddress(user: UserJSON) {
-  return user.email_addresses.find(
-    (emailAddress) => emailAddress.id === user.primary_email_address_id
-  )?.email_address;
-}
